c34d5fac6d8c1dadf36f0fbadc13a110382f468b
suman233 <77207981+suman233@users.noreply.github.com>
2023-08-28 22:45:10 +0530

    Update game.js

--- flipgame/game.js
+++ flipgame/game.js
@@ -4,8 +4,9 @@ console.log(cards);
 let isFlipped = false
 let firstCard;
 let secondCard;
-
-cards.forEach(card=>card.addEventListener("click", flip))
+let count=0
+// document.getElementsByClassName('card')=
+// cards.forEach(card=>card.addEventListener("click", flip))
 function flip(){
     // console.log("card flipped"); 
     // console.log(this);
@@ -14,10 +15,15 @@ function flip(){
         isFlipped=true
         firstCard=this
     }
-    else{
+    else {
         secondCard=this
         check()
     }
+    // else{
+    //     // alert(`You won. Now restart the game agaain`)
+    //     // count++
+    //     alert(`Your number of trials: ${count}`)
+    // }
 }
 
 function check(){
@@ -26,7 +32,7 @@ function check(){
 function success(){
     firstCard.removeEventListener('click',flip)
     secondCard.removeEventListener('click',flip)
-    rest()
+    successReset()
 }
 
 function fail(){
@@ -36,50 +42,42 @@ function fail(){
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
         rest();
-    },1000)
+        alert(`Try again`)
+    },500)
+}
+function successReset(){    
+    isFlipped=false
+    firstCard=null
+    secondCard=null
+    count=count+1
+    console.log(count);
+    // count===8?alert(`Congratualations!! You have won. Now Restart the game to play again`):false
+    if(count=== 8){
+        alert(`Congratualations!! You have won. Now Restart the game to play again`)
+        cards.forEach(card=>card.addEventListener("click", flip))
+        count=0
+    }
+    else
+        false
 }
 
 function rest(){
     isFlipped=false
     firstCard=null
     secondCard=null
+    // if (count===8 ){alert(`you lost`);location.reload()}
 }
-// function restart(){
-//     // document.getElementById(".card").reset();
-//     document.getElementsByClassName('card').addEventListener('click', flip).reset()
-// }
+
 function start(){
     cards.forEach(card=>card.addEventListener("click", flip))
     flip()
-    // const resetBtns=document.querySelector('.restart-btn');
-    // for (var i = 0, len = cards.length; i < len; ++i
-    //     ) {
-    //         let cardImg=Math.floor((i+2)* Math.random()) + 1
-    //         while (!Array.from(new Set([...firstCards,...secondCards])).includes(`img
-    //         ${cardImg}`)){
-    //             cardImg=Math.floor((len* Math.random())) + 1
-    //             };
-    //             cards[i].innerHTML=`<div class="front"><span>${cardImg}</span></
-    //             div><div class="back"></div>`
-    //             cards[i].setAttribute('data-image',`${cardImg}`)
-    //             setTimeout(() => {
-    //                 cards[i].style['backgroundImage']='url("./assets/images/'+ `${cardImg
-    //                     }`+'".jpg)'},50*(i));
-    //                     });
 }
 
 
-// function reset(){
-//     for (let i in cards) {
-//         const element = document.getElementsByClassName(`card${i}`)
-//         element.addEventListener("click", flip, false);
-//         console.log(`${element}: ${cards[i]}`)
-//         // element.setAttribute("data-image", `${cards[i]}`);
-//         };
-// }
 function reset(){
     cards.forEach(card=>card.classList.remove('flip'))
     shuffle()
+    count=0
 }
 
 const resetButton = document.getElementById('restart')
@@ -88,14 +86,6 @@ resetButton.addEventListener('click', reset)
 function shuffle() {
     cards.forEach(card=>{
         let randomIndex=Math.floor((Math.random()*16));
-        // card.style['order']=randomIndex+25%4*(-3)+75
         card.style.order = randomIndex;
     })
 }
-// (function shuffle() {
-//     cards.forEach(card=>{
-//         let randomIndex=Math.floor((Math.random()*16));
-//         // card.style['order']=randomIndex+25%4*(-3)+75
-//         card.style.order = randomIndex;
-//     })
-// })();
